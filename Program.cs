f8b061c2ff55037ee9d8cced405eea2b3d049701
RamiroGarcia1 <ramirogarcia003@gmail.com>
2026-08-22 11:51:19 -0300

    etapa 4: descuento por monto

--- Program.cs
+++ Program.cs
@@ -3,13 +3,15 @@ string nombreCajero = "Ana";
 Console.WriteLine("Nombre del cajero: " + nombreCajero);
 Console.WriteLine($"bienvenida, {nombreCajero}. Caja abierta ");
 
+const decimal PorcentajeDescuento10 = 0.10m;
+const decimal PorcentajeDescuento5 = 0.05m;
 decimal totalVenta = 0;
 int cantidadProductos = 0;
 string opt;
 
 do
 {
-    Console.WriteLine("que desea hacer?");
+    Console.WriteLine("\nque desea hacer?");
     Console.WriteLine("1. cargar producto");
     Console.WriteLine("2. finalizar venta");
     Console.Write("opcion: ");
@@ -20,14 +22,16 @@ do
         case "1":
             Console.Write("ingrese producto: ");
             string nombreProducto = Console.ReadLine();
+
             Console.Write("ingrese precio: ");
             decimal precioProducto = Convert.ToDecimal(Console.ReadLine());
+
             totalVenta += precioProducto;
             cantidadProductos++;
             break;
 
         case "2":
-            Console.WriteLine("cerrando venta");
+            Console.WriteLine("Cerrando la venta..");
             break;
 
         default:
@@ -36,7 +40,24 @@ do
     }
 
 } while (opt != "2");
-Console.WriteLine($"\ncantidad de productos: {cantidadProductos}");
-Console.WriteLine($"total de la venta: ${totalVenta}");
+
+decimal porcentajeAplicado = 0m;
+
+if (totalVenta > 50000m)
+{
+    porcentajeAplicado = PorcentajeDescuento10;
+}
+else if (totalVenta > 20000m)
+{
+    porcentajeAplicado = PorcentajeDescuento5;
+}
+
+decimal descuento = totalVenta * porcentajeAplicado;
+decimal totalFinal = totalVenta - descuento;
+
+Console.WriteLine($"cantidad de productos: {cantidadProductos}");
+Console.WriteLine($"subtotal: ${totalVenta}");
+Console.WriteLine($"descuento aplicado: ${descuento}");
+Console.WriteLine($"total con descuento: ${totalFinal}");
 
 Console.ReadLine();
